2ed1b57d4abe20d64a6991622ea624b591f2b71b
Shubhanshu Kumar Singh <shubhanshusks30@gmail.com>
2021-12-10 15:43:03 +0530

    inital commit

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(NULL);
+#define Shubh cout.tie(NULL);
+#define ll long long
+#define fl(i,n) for(int i=0;i<n;i++)
+#define vec vector<int>
+
+int main(){
+    Code By Shubh
+    int a=4,b=4;
+    cout<<a+b<<endl;
+    return 0;
+}

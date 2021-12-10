3e470cf47ba381fc365e9dbcb1e61de7c302cdad
Shubhanshu Kumar Singh <shubhanshusks30@gmail.com>
2021-12-10 15:53:11 +0530

    adding

---
+++ diff.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(NULL);
+#define Shubh cout.tie(NULL);
+#define ll long long
+#define fl(i,n) for(int i=0;i<n;i++)
+#define vec vector<int>
+
+int main(){
+    Code By Shubh
+    
+    return 0;
+}
